2458d332f6a1289983ff2b624bb177b9104f5d1b
unknown <denis.perrisseau.dev@gmail.com>
2023-02-15 16:42:23 +0100

    Add hover for details on room picture

--- frontend/src/components/RoomHome/RoomsFiltered.jsx
+++ frontend/src/components/RoomHome/RoomsFiltered.jsx
@@ -49,13 +49,23 @@ function RoomsFiltered() {
             key={value.id}
           >
             <article className="overflow-hidden rounded-lg shadow-lg">
-              <a href="/">
-                <img
-                  alt={value.name}
-                  className="block h-auto w-full"
-                  src={`${baseUrl}${value.url_picture}`}
-                />
-              </a>
+              <div className="w-full h-fit group">
+                <div className="relative overflow-hidden">
+                  <img
+                    className="h-auto w-full object-cover"
+                    src={`${baseUrl}${value.url_picture}`}
+                    alt={value.name}
+                  />
+                  <div className="absolute h-full w-full bg-dark-100/30 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
+                    <button
+                      className="bg-greySimple-100 bg-opacity-30 text-whiteSimple-100 py-2 px-5"
+                      type="button"
+                    >
+                      Plus de détails
+                    </button>
+                  </div>
+                </div>
+              </div>
 
               <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-lg">
@@ -82,7 +92,7 @@ function RoomsFiltered() {
                   className="no-underline text-dark-100 hover:text-red-dark"
                   href="/"
                 >
-                  <span className="text-dark-100 font-normal">
+                  <span className="text-white-100 font-normal">
                     <button
                       type="button"
                       className="bg-blueDuck-100  px-4 py-2 rounded-lg "
@@ -90,7 +100,6 @@ function RoomsFiltered() {
                       Réserver
                     </button>
                   </span>
-                  <i className="fa fa-heart" />
                 </a>
               </footer>
             </article>
