7231aaed63b44042b5a367f0dc415e48a49bce40
PERRISSEAU Denis (SNCF RESEAU / INFRAPOLE PAYS DE LA LOIRE / Equipe Spécialiste Inform) <Denis>
2023-02-22 09:22:02 +0100

    UseContext and UseMemo replace by props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,51 @@
 import { Routes, Route } from "react-router-dom";
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import Header from "@components/Header/Header";
 import RoomsFiltered from "@components/RoomHome/RoomsFiltered";
-
 import Myreservations from "@components/MyReservations/MyReservations";
-// import Modal from "@components/Modals/Modal";
-// import ModalBtns from "@components/Modals/ModalBtns";
 import ErrorPage from "@components/error/Error";
 import Footer from "@components/Footer/Footer";
-import SharedContext from "./contexts/Sharedcontext";
+import Modal from "@components/Modals/Modal";
+// import ModalBtns from "@components/Modals/ModalBtns";
 import "./App.css";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
-  const [showModalBtns, setShowModalBtns] = useState(false);
-
-  const contextValues = useMemo(
-    () => ({
-      showModal,
-      setShowModal,
-      showModalBtns,
-      setShowModalBtns,
-    }),
-    [showModal, showModalBtns]
-  );
+  const [showMessage, setshowMessage] = useState("");
+  // const [showModalBtns, setShowModalBtns] = useState(false);
 
   return (
     <div>
       <Header />
       <div className="flex flex-col h-screen">
-        <SharedContext.Provider value={contextValues}>
-          <Routes>
-            <Route exact path="/" element={<RoomsFiltered />} />
-            <Route exact path="/MesReservations" element={<Myreservations />} />
-            <Route path="/erreur" element={<ErrorPage />} />
-            {/* remplacer par une étoile ici */}
-          </Routes>
-        </SharedContext.Provider>
+        <Routes>
+          <Route exact path="/" element={<RoomsFiltered />} />
+          <Route
+            exact
+            path="/mesreservations"
+            element={
+              <Myreservations
+                isVisible={showModal}
+                setShowModal={setShowModal}
+                setshowMessage={setshowMessage}
+              />
+            }
+          />
+          <Route path="/erreur" element={<ErrorPage />} />
+          {/* remplacer par une étoile ici */}
+        </Routes>
         <Footer />
-
-        {/* Modales exemples 
-      <button type="button" onClick={() => setShowModal(true)}>
-        Je suis le bouton modal
-        <Home />
-      </button>
-      <button type="button" onClick={() => setShowModalBtns(true)}>
-        Je suis le bouton modal Bouton
-      </button>
-      <Modal
-        isVisible={showModal}
-        onClose={() => setShowModal(false)}
-        message="Votre réservation a bien été prise en compte, un email de confirmation vient de vous être envoyé"
-      />
-      <ModalBtns
-        isVisible={showModalBtns}
-        onClose={() => setShowModalBtns(false)}
-        message="Etes-vous sûr(e) de vouloir supprimer cette réservation ?"
-      /> */}
+        <Modal
+          isVisible={showModal}
+          onClose={() => setShowModal(false)}
+          message={showMessage}
+        />
+        {/*
+        <ModalBtns
+          isVisible={showModalBtns}
+          onClose={() => setShowModalBtns(false)}
+          message="Etes-vous sûr(e) de vouloir supprimer cette réservation ?"
+        /> */}
       </div>
     </div>
   );
