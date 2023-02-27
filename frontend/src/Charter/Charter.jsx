c34890001b6b4387f360eafb0bc0b882e67e081e
Albja <alban.jaunet@sncf.fr>
2023-02-27 09:38:08 +0100

    final push of feature/login

---
+++ frontend/src/Charter/Charter.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+
+function Charter() {
+  return (
+    <div>
+      <p>
+        Introduction :La protection de votre vie privée est une priorité
+        essentielle, et nous utilisons ces données pour vous fournir les
+        produits que vous achetez sur nos sites. Sinon, vous ne pourriez pas
+        acheter vos titres de transport et abonnements ! A ce titre, voici les
+        huit principes auxquels veillent l’entreprise GD_Salles et l’ensemble de
+        ses collaborateurs : Ne jamais collecter ou n’exploiter de données sans
+        une finalité précise et légitime ; Ne collecter des données que dans
+        l’un des cadres suivants : Les données sont nécessaires à l’exécution
+        des contrats et des prestations demandées Vous avez donné votre
+        consentement exprès et spécifique au traitement Nous avons un intérêt
+        légitime dûment justifié pour traiter vos données Ne collecter et
+        n’exploiter que les données strictement nécessaires aux traitements ; Ne
+        conserver les données que pour une durée limitée et cohérente avec les
+        services fournis ; Assurer en permanence la qualité et la sécurité des
+        données ; Ne réaliser de transferts de données que quand cela est
+        nécessaire, et dans le respect des règles légales et de sécurité ; Vous
+        fournir en permanence une information claire et transparente ; Veiller
+        au respect de vos droits et des conditions de leur exercice. La présente
+        charte de confidentialité s’applique à l’ensemble du site, application
+        mobile et services GD_Salles (ci-après les « services GD_Salles »).
+        GD_Salles est une société indépendante de GD_Salles , et, de fait, ces
+        deux sociétés sont responsables de leurs propres traitements de données
+        personnelles. GD_Salles a pour objet professionelle, la recherche de
+        salles de transpformations et leur réservation sous l’empire de la
+        présente charte de confidentialité.
+      </p>
+      <p>
+        Quelle est la politique de GD_Salles en matière de cookies ? Quelle est
+        la politique de GD_Salles en matière de cookies ? Qu’est-ce qu’un cookie
+        ? Ce sont des fichiers déposés dans le navigateur de votre ordinateur ou
+        terminal lors de votre navigation sur les services GD_Salles . Ces
+        fichiers sont par la suite envoyés automatiquement par le navigateur aux
+        serveurs de GD_Salles lors de votre navigation afin : De permettre la
+        navigation sur nos services (sessions de connexion, preuves
+        d’authentification pour la fonction « se souvenir de moi », serveurs
+        utilisés, etc.) ; De suivre la navigation des clients sur les sites,
+        application et newsletters pour en assurer l’optimisation ; De
+        personnaliser les parcours et les offres ; De gérer la publicité sur les
+        services GD_Salles et en dehors. Par qui sont déposés les cookies ? Ces
+        fichiers sont déposés : Soit par GD_Salles Soit par des tiers : Lorsque
+        vous naviguez sur nos services, ou à l’ouverture de nos newsletters, des
+        cookies sont émis par des sociétés tierces (société assurant l’aide des
+        utilisateurs, prestataires de publicité, agence de communications,
+        sociétés de mesures d’audience, réseaux sociaux, etc.), sous réserve des
+        choix que vous avez pu exprimer antérieurement auprès de leurs services,
+        ou à tout moment, dans les conditions décrites ci-après. Ces cookies ont
+        essentiellement pour objectif de vous afficher des contenus
+        publicitaires susceptibles de correspondre à vos centres d’intérêt grâce
+        aux données récoltées lors de votre navigation sur nos services ou sur
+        du site externes ou réseaux sociaux. Ces cookies peuvent ainsi permettre
+        de : Comptabiliser le nombre d’affichages des contenus publicitaires
+        diffusés via les espaces publicitaires de nos services, d’identifier les
+        publicités affichées et le nombre d’utilisateurs ayant cliqué sur chaque
+        publicité, et ce afin de calculer les sommes dues et d’établir des
+        statistiques ; Reconnaître votre ordinateur ou terminal lors de votre
+        navigation sur tout autre site afin d’adapter les publicités qui y sont
+        diffusées. L’émission, l’utilisation et la gestion de ces cookies tiers
+        sont soumises aux politiques de protection des données et de
+        confidentialité de ces sociétés tierces. Toutefois, nous vous informons,
+        quand nous en avons effectivement connaissance, de l’objet et des moyens
+        de gestion de ces cookies. Par ailleurs, si votre ordinateur ou terminal
+        est utilisé par plusieurs personnes, ou s’il dispose de plusieurs
+        logiciels de navigation, il n’est pas possible pour GD_Salles de vous
+        assurer que les services et publicités correspondent bien à votre
+        utilisation de nos services et non à celle d’un autre utilisateur. Ce
+        choix de partage et de configuration relève de votre libre choix et
+        responsabilité. Descriptif détaillé des cookies GD_Salles : Cookies à
+        usages techniques : Sessions de navigation (connexion compte client,
+        session technique de navigation, serveurs utilisés) Simplification de la
+        visite avec adaptation aux critères d’affichage de votre ordinateur ou
+        terminal (langue utilisée, résolution d’affichage, système
+        d’exploitation, navigateur utilisé, paramètre d’accessibilité) Détection
+        d’une visite précédente Enregistrement des préférences clients
+        Reconnexion automatique au compte client (quand activé) Ces cookies sont
+        nécessaires au bon fonctionnement du site GD_Salles et à votre
+        navigation, et ne peuvent donc faire l’objet d’un paramétrage. Cookies à
+        usage de suivi : Mesures d’audience : données statistiques de
+        fréquentation et d’utilisation du site de GD_Salles (rubriques et
+        contenus visés, parcours) afin de mesurer et étudier le fonctionnement
+        et l’efficacité de nos services, et ainsi d’en améliorer l’intérêt et
+        l’ergonomie. Tests comparés de plusieurs versions du site (AB Testing)
+        afin d’améliorer l’intérêt et l’ergonomie de nos services. Conformément
+        aux recommandations de la CNIL en vigueur, certains cookies de mesure
+        d’audience sont exemptés de consentement, dans la mesure où sont
+        respectées les mesures suivantes : Les cookies ont une finalité
+        strictement limitée à la mesure de l’audience de nos sites et
+        application, pour le compte exclusif de GD_Salles , Les cookies ont
+        uniquement pour objectif de produire des données statistiques anonymes,
+        ainsi ils ne permettent pas un suivi global de la navigation et ne sont
+        pas transmis à des tiers. Cookies à fins de personnalisation :
+        Personnalisation en temps réel de la navigation sur nos services et
+        proposition d’offres pertinentes et personnalisées Amélioration de
+        l’ergonomie Identification de problèmes de navigation afin de proposer
+        l’assistance d’un téléconseiller Cookies à usage publicitaire : Comptage
+        et personnalisation des contenus et publicités proposés lorsque vous
+        visitez les services GD_Salles Affichages publicitaires sur du site
+        externes ou réseaux sociaux Comment sont-ils mis en place ? Conformément
+        aux recommandations de la CNIL, en navigant sur nos services, vous êtes
+        informé de l’existence des cookies via le bandeau de navigation
+        apparaissant lors de votre première visite ou en cas d’effacement des
+        cookies déposés par GD_Salles . Comment exprimer mes choix concernant
+        les cookies ? Vous avez la possibilité de paramétrer votre navigation :
+        Soit via le menu de paramétrage (gestion des cookies) Soit avec votre
+        navigateur : vous pouvez autoriser ou refuser l’enregistrement de
+        cookies avec votre ordinateur ou votre terminal avec les paramètres
+        appropriés de votre logiciel de navigation. Ces paramètres sont propres
+        à chaque navigateur, et sont accessibles via le menu d’aide de votre
+        navigateur Internet Explorer™ :
+        http://windows.microsoft.com/fr-FR/windows-vista/Block-or-allow-cookies
+        Pour Safari™ : https://support.apple.com/kb/ph21411?locale=fr_CA Pour
+        Chrome™ :
+        http://support.google.com/chrome/bin/answer.py?hl=fr&hlrm=en&answer=95647
+        Pour Firefox™ :
+        http://support.mozilla.org/fr/kb/Activer%20et%20d%C3%A9sactiver%20les%20cookies
+        Pour Opera™ : http://help.opera.com/Windows/10.20/fr/cookies.html
+      </p>
+    </div>
+  );
+}
+
+export default Charter;
