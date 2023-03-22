f0f3ce864453c83483b11dfdf6f342ee116bbf99
DenisWCS1 <denis.perrisseau.dev@gmail.com>
2023-03-22 19:53:20 +0100

    Clean bug on load google map, refresh loose font path and loader on roomdetails

--- frontend/src/components/Loader/Loader.jsx
+++ frontend/src/components/Loader/Loader.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import loader from "@assets/loader/loading.gif";
 
 function Loader() {
   return (
     <div className="flex flex-col justify-around items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <img className="animate" src="loading.gif" alt="loading" />
+      <img className="animate" src={loader} alt="loading" />
       <p className="text-center font-bold">Chargement ...</p>
     </div>
   );
